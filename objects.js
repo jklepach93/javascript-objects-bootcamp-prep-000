91e562e0fb316ff8cb36f38ef13f458ac431ef57
Justin Klepach <justin.klepach93@gmail.com>
2017-12-06 07:05:28 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -5,6 +5,6 @@ function updatePlaylist (playlist,artist,song) {
   return Object.assign({}, playlist, { [artist]: song })
 }
 function removeFromPlaylist(playlist,artist) {
-  return Object.assign({}, playlist, { [artist]:})
   Delete removeFromPlaylist.artist
+  removeFromPlaylist
 }
